f04d34a38ccb0ceaef8a7d6f53a8c17dea0a3ad8
Lars Jansøn Engvik <lars@engvik.nu>
2020-09-17 20:36:10 +0200

    feature(config): add validation

--- config.go
+++ config.go
@@ -1,8 +1,31 @@
 package sbanken
 
+type ConfigError struct {
+	ErrorString string
+}
+
+func (ce *ConfigError) Error() string {
+	return ce.ErrorString
+}
+
+var (
+	ErrNoClientID    = &ConfigError{"No client ID"}
+	ErNoClientSecret = &ConfigError{"No client secret"}
+)
+
 type Config struct {
+	ClientID     string
+	ClientSecret string
 }
 
 func (c *Config) validate() error {
+	if c.ClientID == "" {
+		return ErrNoClientID
+	}
+
+	if c.ClientSecret == "" {
+		return ErNoClientSecret
+	}
+
 	return nil
 }
