535243849b27e9ad42faad618c7908fb4c3f2118
Lars Jansøn Engvik <lars@engvik.nu>
2020-09-20 17:01:21 +0200

    feature(sbanken): authorize client

--- sbanken.go
+++ sbanken.go
@@ -1,11 +1,27 @@
 package sbanken
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+	"time"
 )
 
 type Client struct {
-	HTTP *http.Client
+	HTTP   *http.Client
+	config *Config
+	auth   auth
+}
+
+type auth struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	Scope       string `json:"scope"`
+	ExpiresIn   int    `json:"expires_in"`
+	expires     time.Time
 }
 
 func NewClient(cfg *Config, httpClient *http.Client) (*Client, error) {
@@ -15,8 +31,11 @@ func NewClient(cfg *Config, httpClient *http.Client) (*Client, error) {
 
 	c := &Client{}
 	c.setHTTPClient(httpClient)
+	c.config = cfg
 
-	c.authorize()
+	if err := c.authorize(cfg); err != nil {
+		return nil, err
+	}
 
 	return c, nil
 }
@@ -30,5 +49,56 @@ func (c *Client) setHTTPClient(httpClient *http.Client) {
 	c.HTTP = httpClient
 }
 
-func (c *Client) authorize() {
+func (c *Client) authorize(cfg *Config) error {
+	authURL := "https://auth.sbanken.no/identityserver/connect/token"
+	payload := []byte("grant_type=client_credentials")
+
+	req, err := http.NewRequest(http.MethodPost, authURL, bytes.NewBuffer(payload))
+	if err != nil {
+		return err
+	}
+
+	req.SetBasicAuth(url.QueryEscape(cfg.ClientID), url.QueryEscape(cfg.ClientSecret))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=utf-8")
+	req.Header.Set("Accept", "application/json")
+
+	res, err := c.HTTP.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		// TODO: Handle error from body
+		return fmt.Errorf("unexpected status code: %d", res.StatusCode)
+	}
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	var a auth
+	if err := json.Unmarshal(data, &a); err != nil {
+		return err
+	}
+
+	exp := time.Now().Add(time.Second * time.Duration(a.ExpiresIn))
+	a.expires = exp
+
+	c.auth = a
+
+	return nil
+}
+
+func (c *Client) getToken() (string, error) {
+	if time.Now().After(c.auth.expires) {
+		err := c.authorize(c.config)
+		if err != nil {
+			return "", fmt.Errorf("error renewing token: %w", err)
+		}
+	}
+
+	return c.auth.AccessToken, nil
 }
--- sbanken_test.go
+++ sbanken_test.go
@@ -2,13 +2,20 @@ package sbanken
 
 import (
 	"log"
+	"os"
 	"testing"
 )
 
 func TestNewClient(t *testing.T) {
-	c, err := NewClient(&Config{}, nil)
+	cfg := &Config{
+		ClientID:     os.Getenv("CLIENT_ID"),
+		ClientSecret: os.Getenv("CLIENT_SECRET"),
+	}
+
+	c, err := NewClient(cfg, nil)
 	if err != nil {
 		t.Fatalf("error setting up test: %v", err)
 	}
-	log.Println(c)
+
+	log.Println(c.getToken())
 }
